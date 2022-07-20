25a9973090171ed66ed9a1d7804cbbd29190af0d
enochlee <97347626+iamenochlee@users.noreply.github.com>
2022-07-19 18:06:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 
+    var menuBtn = document.getElementById("menuBtn");
+    var sideNav = document.getElementById("sideNav");
+
+    sideNav.style.display = "none";
+    menuBtn.onclick = function() {
+        if (sideNav.style.display = "none") {
+            sideNav.style.display = "block";
+            
+        }
+        
+        else {
+            sideNav.style.display = "none";
+        }
+    }
+    
+    
+    
